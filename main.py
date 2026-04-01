7fc949d8f35543a62213170df130faf4f754b619
Rafael Vasconcelos <rmxvrelease@gmail.com>
2026-04-01 18:40:21 -0300

    feature: criação de exemplos em código

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from matplotlib.axes import Axes
 from typing import Optional
 import json
 from pathlib import Path
 import matplotlib.pyplot as plt
 
 class TrainingSample:
-    def __init__(self, plaintext: str, comment: Optional[str], language: str, cipher_key: str):
-        self.plaintext=plaintext
-        self.comment=comment
-        self.language=language
-        self.cipher_key=cipher_key
+    def __init__(self, plaintext: str, comment: Optional[str], language: str, cipher_key: str, cipher_text: bytes):
+        self.plaintext:str=plaintext
+        self.comment:Optional[str]=comment
+        self.language:str=language
+        self.cipher_key:str=cipher_key
+        self.cipher_text:bytes=cipher_text
 
     
 def vigenere_cipher(
@@ -20,7 +22,6 @@ def vigenere_cipher(
     plain_text_bytes = plain_text.encode('utf-8') if isinstance(plain_text, str) else plain_text
     cipher_text_bytes: list[int] = []
     for (plain_text_byte, key_byte) in zip(plain_text_bytes, extended_key_bytes):
-        print(plain_text_byte, key_byte, key_byte ^ plain_text_byte)
         cipher_text_bytes.append(plain_text_byte ^ key_byte)
     return bytes(cipher_text_bytes)
 
@@ -33,26 +34,43 @@ def letter_distribuition(text: bytes | str) -> dict[int, int]:
     return frequency_dict
 
 
-def show_letter_distribuition(text: bytes):
-    plt.hist(text, 256)
-    plt.show()
-
+def make_letter_distribuition_panel(text: bytes, ax: Axes, title: Optional[str]=None):
+    ax.hist([b for b in text], bins=256)
+    if title:
+        ax.set_title(title)
 
 
 def get_nth_data_sample(n: int) -> TrainingSample:
     data_path = Path(__file__).parent / 'training_data.json'
     with open(data_path, 'rb') as f:
         sample_file_data = json.load(f)
+        plain_text = sample_file_data[n].get("plaintext")
+        cipher_key = sample_file_data[n].get("cipher_key")
+        cipher_text = vigenere_cipher(plain_text, cipher_key)
         return TrainingSample(
-            sample_file_data[n].get("plaintext"),
+            plain_text,
             sample_file_data[n].get("comment"),
             sample_file_data[n].get("language"),
-            sample_file_data[n].get("cipher_key")
+            cipher_key,
+            cipher_text=cipher_text
         )
 
 def main():
-    print(vigenere_cipher(vigenere_cipher("Cifrar duas vezes equivale a não fazer nada", 'ab'), 'ab'))
-    print(get_nth_data_sample(0).plaintext)
+    # exemplificação do uso básico da cifra
+    plain_text="Cifra legal e legível"
+    cipher="batata"
+    print(f"plaintext: {plain_text}")
+    print(f"cifra: {cipher}")
+    print(f"cipher text: {vigenere_cipher(plain_text, cipher)}")
+
+    # exemplificação da reversibilidade da cifra
+    print(vigenere_cipher(vigenere_cipher("Cifrar duas vezes equivale a não fazer nada", 'ab'), 'ab').decode('utf-8'))
+    
+    sample = get_nth_data_sample(0)
+    axes: Axes = plt.subplots(2, 1)[1]
+    make_letter_distribuition_panel(sample.plaintext.encode('utf-8'), axes[0], "plaintext")
+    make_letter_distribuition_panel(sample.cipher_text, axes[1], "ciphertext")
+    plt.show()
 
 
 if __name__ == "__main__":
